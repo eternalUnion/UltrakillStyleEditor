e5bc061c9f3817a8b731fb1b63860151adc33a3e
eternalUnion <dnzbyk12345@gmail.com>
2023-06-02 21:24:21 +0300

    Add project files.

---
+++ UltrakillStyleEditor/Plugin.cs
@@ -0,0 +1,18 @@
+﻿using BepInEx;
+
+namespace UltrakillStyleEditor
+{
+    [BepInPlugin(PLUGIN_GUID, PLUGIN_NAME, PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        public const string PLUGIN_NAME = "StyleEditor";
+        public const string PLUGIN_GUID = "eternalUnion.ultrakill.styleEditor";
+        public const string PLUGIN_VERSION = "1.0.0";
+
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin {PLUGIN_GUID} is loaded!");
+        }
+    }
+}
